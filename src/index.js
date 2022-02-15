c6d66b6d84d2a0beb6255756e15cfb93dd307864
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-02-15 13:52:12 +0100

    Create todo app base code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './style.css';
+
+const todoHTML = document.querySelector('.todo_list');
+
+let todoTasks = [
+  {
+    desctiption: 'string',
+    completed: 'bool',
+    index: 'number',
+  },
+  {
+    desctiption: 'string',
+    completed: 'bool',
+    index: 'number',
+  },
+  {
+    desctiption: 'string',
+    completed: 'bool',
+    index: 'number',
+  },
+];
+
+todoTasks.forEach((task, i) => {
+  task.index = i;
+  todoHTML.innerHTML += `${task.desctiption} <br> ${task.completed} <br> ${task.index}`;
+});
