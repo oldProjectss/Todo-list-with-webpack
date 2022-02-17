fe6fd322e660b9eaea20dcea3563768951407769
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-02-17 08:58:29 +0100

    Fix linters on index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style.css';
 import '../node_modules/@fortawesome/fontawesome-free/js/all.js';
-import { addTask, removeTask, editTask, setLocalStore } from './functionality.js';
+import {
+  addTask, removeTask, editTask, setLocalStore,
+} from './functionality.js';
 import isCompleted from './check.js';
 
 const todoHTML = document.querySelector('.todo_list');
